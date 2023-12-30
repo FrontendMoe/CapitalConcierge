664c5be77d909230e126af93f9c5722ff52af8da
frontendmoe <frontendmoe@gmail.com>
2023-12-30 22:07:29 +0100

    InitialCommit

--- concierge/src/App.jsx
+++ concierge/src/App.jsx
@@ -2,13 +2,90 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import './fonts/stylesheet.css'
+import video from './vid.mp4'
 function App() {
   const [count, setCount] = useState(0)
-
+  const nav = [
+    {
+      name: 'CONTACT',
+      link: 'CONTACT',
+    },
+    {
+      name: 'Services',
+      link: 'Services',
+    },
+    {
+      name: 'COMPANY',
+      link: 'COMPANY',
+    },
+    {
+      name: 'WELCOME',
+      link: 'WELCOME',
+    },
+  ]
+  function scrollToSection(sectionId, margin = 50) {
+    var section = document.getElementById(sectionId)
+    if (section) {
+      var sectionTop = section.offsetTop
+      window.scrollTo({
+        top: sectionTop - margin,
+        behavior: 'smooth',
+      })
+    }
+  }
   return (
-    <div className="bg-[#F6C94B] text min-h-screen text-black">
-      <div className="py-[56px] border-b-[3px] border-black "></div>
+    <div>
+      <div className="bg-[#F6C94B]  flex flex-col text min-h-screen text-black">
+        <div className="py-[56px] flex space-x-[83px] justify-center border-b-[3px] border-black ">
+          {nav.map((el) => (
+            <div
+              key={el}
+              className="cursor-pointer uppercase font-[600]"
+              onClick={() => scrollToSection(el.link)}
+            >
+              {el.name}
+            </div>
+          ))}
+        </div>
+        <div className="  flex-1 w-full h-full   flex">
+          <div className="bg-[#F6C94B] space-y-[72px]   py-[128px] px-[150px]  border-black   border-r-[3px]  w-1/2 ">
+            <p className="text-[84px] max-w-[400px] Hiragino leading-[89px]">
+              The Best For Your Guest.
+            </p>
+            <div className="w-[156px] h-[3px] bg-black"></div>
+            <p className="text-[25px] leading-[37px] max-w-[330px] font-[300]">
+              Professional & Innovative Concierge Services & Solutions.
+            </p>
+          </div>
+          <div className="bg-[#F6C94B]  w-1/2 ">
+            <video
+              style={{ mixBlendMode: 'multiply' }}
+              className="h-full object-cover object-center"
+              autoPlay
+              muted
+              src={video}
+              loop
+            ></video>
+          </div>
+        </div>
+      </div>
+      <div className="text-center border-black border-t-[3px] space-y-[70px] bg-[#FFF7DF] py-[160px]">
+        <p className="text-[52px] leading-[61px] Hiragino  font-[600]">
+          CAPITAL <br />
+          CONCIERGE
+        </p>
+        <div className="w-[341px] mx-auto bg-black h-[3px]"></div>
+        <p className="w-[890px] mx-auto text-center text-black text-[25px] font-light font-['Gibson'] leading-[58px] tracking-wide">
+          We provide each client the personal attention and consideration they
+          need, including but not limited to; residential, homeowners
+          associations, commercial properties and hospitality companies. We take
+          the values and requests of our clients' very seriously, providing only
+          the very best in customer service..
+        </p>
+
+        <div className="w-[156px]  mx-auto bg-black h-[3px]"></div>
+      </div>
     </div>
   )
 }
