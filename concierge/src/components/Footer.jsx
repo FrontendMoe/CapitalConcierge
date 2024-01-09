39d03f01298f9c9085958b3e1faa575735e5e3fa
frontendmoe <frontendmoe@gmail.com>
2024-01-09 20:19:55 +0100

    Fixes

--- concierge/src/components/Footer.jsx
+++ concierge/src/components/Footer.jsx
@@ -1,5 +1,18 @@
 function Footer() {
-  return <footer className="bg-[#282724] min-h-[30vh]"></footer>
+  return (
+    <footer className="bg-[#282724] min-h-[30vh] py-[100px] lg:py-[160px]">
+      <div className="xl:max-w-[1023px] lg:px-[160px] px-[30px] xl:mx-auto xl:px-0 text-orange-100 text-[16px] lg:text-[25px] font-light font-['Gibson'] leading-[44.70px]">
+        Professional & Innovative Concierge Services & Solutions.
+        <br />
+      </div>{' '}
+      <br />
+      <div className="xl:max-w-[1023px] lg:px-[160px] px-[30px] xl:mx-auto xl:px-0 text-orange-100 text-[16px] lg:text-[25px] font-light font-['Gibson'] leading-[44.70px]">
+        Janell, and her team are attentive, professional, kind and most
+        importantly to me, because I am a senior, they make me feel safe and
+        cared for both inside our building <br />
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- concierge/src/components/Work.jsx
+++ concierge/src/components/Work.jsx
@@ -1,25 +1,27 @@
 function Work() {
   return (
-    <section className="px-[30px] lg:px-[150px] bg-[#F6C94B] space-y-[15px] lg:space-y-[27px] py-[50px] lg:py-[120px]">
-      <p className="max-w-[939px]  Hiragino text-stone-800 text-[30px] lg:text-[84px] font-[400] lg:font-light font-['Hiragino Mincho ProN'] lg:leading-[82.30px]">
-        Let’s Work Together.
-      </p>
-      <p className="max-w-[923px]    text-black text-[20px] lg:text-[25px] font-light font-['Gibson'] lg:leading-[44.70px]">
-        Janell, and her team are attentive, professional, kind and most
-        importantly to me, because I am a senior, they make me feel safe and
-        cared for both inside our building <br />
-      </p>
-      <button className="max-w-[397px] w-full flex items-center h-[69px] bg-[#282724]">
-        <p className="w-full lg:w-[417.61px] flex justify-between px-[25px]">
-          <span className="text-orange-100 text-[17px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-wide">
-            CONTACT US{' '}
-          </span>
-          <span className="text-amber-300 text-[20px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-wide">
-            {`>`}
-          </span>
+    <div className="bg-[#F6C94B] w-full">
+      <section className="px-[30px] xl:max-w-[1023px] xl:mx-auto xl:px-0 lg:px-[150px]  space-y-[15px] lg:space-y-[27px] py-[50px] lg:py-[120px]">
+        <p className="max-w-[939px]  Hiragino text-stone-800 text-[30px] lg:text-[84px] font-[400] lg:font-light font-['Hiragino Mincho ProN'] lg:leading-[82.30px]">
+          Let’s Work Together.
         </p>
-      </button>
-    </section>
+        <p className="max-w-[923px]    text-black text-[20px] lg:text-[25px] font-light font-['Gibson'] lg:leading-[44.70px]">
+          Janell, and her team are attentive, professional, kind and most
+          importantly to me, because I am a senior, they make me feel safe and
+          cared for both inside our building <br />
+        </p>
+        <button className="max-w-[397px] w-full flex items-center h-[69px] bg-[#282724]">
+          <p className="w-full lg:w-[417.61px] flex justify-between px-[25px]">
+            <span className="text-orange-100 text-[17px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-wide">
+              CONTACT US{' '}
+            </span>
+            <span className="text-amber-300 text-[20px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-wide">
+              {`>`}
+            </span>
+          </p>
+        </button>
+      </section>
+    </div>
   )
 }
 
