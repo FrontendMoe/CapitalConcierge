afdf8df4f011f4cb6ab0a2fdedbcff257ccf7089
frontendmoe <frontendmoe@gmail.com>
2024-01-11 17:34:12 +0100

    Fixes

--- concierge/src/App.jsx
+++ concierge/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-
-import './App.css'
 import './fonts/stylesheet.css'
+import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Company from './pages/Company'
