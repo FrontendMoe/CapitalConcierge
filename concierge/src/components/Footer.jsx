a97d0a20d974913f7cda216682e43bf51c6c30c9
frontendmoe <frontendmoe@gmail.com>
2024-01-11 01:55:00 +0100

    Fixes

--- concierge/src/components/Footer.jsx
+++ concierge/src/components/Footer.jsx
@@ -1,15 +1,22 @@
+// import { FooterLogo } from '../assets/icons'
+import Footerlogo from '../assets/FooterLogo.png'
 function Footer() {
   return (
-    <footer className="bg-[#282724] min-h-[30vh] py-[100px] lg:py-[160px]">
-      <div className="xl:max-w-[1023px] lg:px-[160px] px-[30px] xl:mx-auto xl:px-0 text-orange-100 text-[16px] lg:text-[25px] font-light font-['Gibson'] leading-[44.70px]">
-        Professional & Innovative Concierge Services & Solutions.
-        <br />
-      </div>{' '}
-      <br />
-      <div className="xl:max-w-[1023px] lg:px-[160px] px-[30px] xl:mx-auto xl:px-0 text-orange-100 text-[16px] lg:text-[25px] font-light font-['Gibson'] leading-[44.70px]">
-        Janell, and her team are attentive, professional, kind and most
-        importantly to me, because I am a senior, they make me feel safe and
-        cared for both inside our building <br />
+    <footer className="bg-[#282724] flex lg:flex-row flex-col items-center  min-h-[30vh] py-[100px] lg:py-[160px]">
+      <div>
+        {' '}
+        <img src={Footerlogo} className="w-[187px]" alt="" />{' '}
+      </div>
+      <div>
+        <p className="max-w-[255.68px] lg:hidden text-center">
+          <span className="text-orange-100 text-[22.67px] font-semibold font-['Gibson'] leading-loose">
+            Capital Concierge LLC. © 2023
+            <br /> <br />
+          </span>
+          <span className="text-orange-100 text-[22.67px] font-light font-['Gibson'] leading-loose">
+            Professional & Innovative Concierge Services & Solutions.
+          </span>
+        </p>
       </div>
     </footer>
   )
--- concierge/src/components/Work.jsx
+++ concierge/src/components/Work.jsx
@@ -1,15 +1,25 @@
 function Work() {
   return (
-    <div className="bg-[#F6C94B] w-full">
-      <section className="px-[30px] xl:max-w-[1023px] xl:mx-auto xl:px-0 lg:px-[150px]  space-y-[15px] lg:space-y-[27px] py-[50px] lg:py-[120px]">
-        <p className="max-w-[939px]  Hiragino text-stone-800 text-[30px] lg:text-[84px] font-[400] lg:font-light font-['Hiragino Mincho ProN'] lg:leading-[82.30px]">
+    <div className="bg-[#F6C94B] -mt-[1px] w-full">
+      <section className="px-[70px] xl:max-w-[1023px] xl:mx-auto xl:px-0 lg:px-[150px]  space-y-[40px] lg:space-y-[27px] py-[60px] lg:py-[120px]">
+        <p className="max-w-[939px] lg:block hidden  Hiragino text-stone-800 text-[30px] lg:text-[84px] font-[400] lg:font-light font-['Hiragino Mincho ProN'] lg:leading-[82.30px]">
           Let’s Work Together.
         </p>
-        <p className="max-w-[923px]    text-black text-[20px] lg:text-[25px] font-light font-['Gibson'] lg:leading-[44.70px]">
+        <p className="Hiragino max-w-[243px] lg:hidden  text-zinc-800 text-[58.04px] font-light font-['Hiragino Mincho ProN'] leading-[61.50px]">
+          Let’s Work Together.
+        </p>
+
+        <p className="max-w-[923px] lg:block hidden    text-black text-[20px] lg:text-[25px] font-light font-['Gibson'] lg:leading-[44.70px]">
           Janell, and her team are attentive, professional, kind and most
           importantly to me, because I am a senior, they make me feel safe and
           cared for both inside our building <br />
         </p>
+        <div className="max-w-[261px] lg:hidden  text-black text-xl font-light font-['Gibson'] leading-9">
+          Janell, and her team are attentive, professional, kind and most
+          importantly to me, because I am a senior, they make me feel safe and
+          cared for both inside our building <br />
+        </div>
+
         <button className="max-w-[397px] w-full flex items-center h-[69px] bg-[#282724]">
           <p className="w-full lg:w-[417.61px] flex justify-between px-[25px]">
             <span className="text-orange-100 text-[17px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-wide">
