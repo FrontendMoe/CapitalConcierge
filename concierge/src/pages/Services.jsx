383da07a8750390fa59dc3e1dc69132d0627f115
frontendmoe <frontendmoe@gmail.com>
2024-04-19 01:24:22 +0100

    Fixes

--- concierge/src/pages/Services.jsx
+++ concierge/src/pages/Services.jsx
@@ -7,7 +7,8 @@ import keys from '../assets/keys.svg'
 import moving from '../assets/moving.svg'
 import Doorman from '../assets/Doorman.svg'
 import { mailTo } from '../utils/functions'
-
+import Fire from '../assets/Fire.svg'
+import { Link } from 'react-router-dom'
 function Services() {
   const offers = [
     {
@@ -106,14 +107,14 @@ function Services() {
           </div>
         </div>
       </section>
-      <section className="px-[50px] lg:px-[220px] pt-[50px] lg:pt-[106px] space-y-[30px] lg:space-y-[95px]  bg-[#FFF7DF]">
-        <div id="Offering">
+      <section className="  lg:px-[220px] pt-[50px] lg:pt-[106px] space-y-[30px] lg:space-y-[95px]  bg-[#FFF7DF]">
+        <div className="px-[50px]" id="Offering">
           <div className=" lg:block hidden Hiragino text-[#7D5555] text-[80px] font-light font-['Hiragino Mincho ProN'] leading-[82.30px]">
             Offerings & Services.
           </div>
         </div>
 
-        <div className="grid lg:gap-0 lg:grid-cols-3">
+        <div className="grid px-[50px] lg:gap-0 lg:grid-cols-3">
           {offers.map((el, index) => (
             <div
               key={index}
@@ -141,7 +142,7 @@ function Services() {
               </div>
             </div>
           ))}
-          <div className="bg-[#282724] lg:hidden relative z-20 space-y-[91px]  lg:space-y-[62px] p-[50px] lg:px-[109px] lg:py-[90px]">
+          {/* <div className="bg-[#282724] lg:hidden relative z-20 space-y-[91px]  lg:space-y-[62px] p-[50px] lg:px-[109px] lg:py-[90px]">
             <div className="space-y-[32px] lg:space-y-[54px]">
               <div className="lg:block hidden Hiragino max-w-[734px] text-amber-300 text-[80px] font-light font-['Hiragino Mincho ProN'] leading-[82.30px]">
                 Pricing
@@ -181,48 +182,83 @@ function Services() {
                 REQUEST PRICING {'>'}
               </p>
             </button>
-          </div>
+          </div> */}
         </div>
-        <div className="bg-[#282724] lg:block hidden relative z-20 space-y-[91px]  lg:space-y-[62px] p-[50px] lg:px-[109px] lg:py-[90px]">
-          <div className="space-y-[32px] lg:space-y-[54px]">
-            <div className="lg:block hidden Hiragino max-w-[734px] text-amber-300 text-[80px] font-light font-['Hiragino Mincho ProN'] leading-[82.30px]">
-              Pricing
+        <div className=" lg:py-[60px] relative border-x-[#F4E8CF] lg:px-[30px] border-x">
+          <div className="absolute border-y border-[#F4E8CF]  top-[30px] w-[120%] -left-[10%]"></div>
+          <div className="relative  z-10 flex pb-[184px] pt-[106px] lg:py-[55px] lg:pr-0 pl-[63px] pr-[108px] lg:pl-[369px] bg-[#F5EBCC]">
+            <img
+              src={Fire}
+              alt=""
+              className=" lg:w-fit w-[206.551px] h-[376.575px]  lg:h-fit -right-[80px] lg:right-auto lg:left-[17.7px] bottom-[55px] lg:top-1/2 lg:-translate-y-1/2 absolute "
+            />
+            <div className="space-y-[24px]">
+              <div className="space-y-[32px]">
+                <p className="Hiragino font-[600] leading-[26.302px] text-[30.92px]  text-[#7D5555]">
+                  FIRE SAFETY
+                </p>
+                <p className="text-[#282724]  max-w-[225px]  text-[17.535px] lg:max-w-[596px] font-[300] leading-[31.5px]">
+                  {' '}
+                  NFPA Standard LS.01.02.01 requires that facilities provide
+                  protection for occupants of the building during the time the
+                  Life Safety Code is not met, including during maintenance or
+                  construction.{' '}
+                </p>
+              </div>
+              <div>
+                <Link to="/fire-safety">
+                  <button className="flex text-[23px] font-[600] tracking-[0.46px] text-white leading-[29.7px] w-[265.233px] lg:w-[397px] items-center h-[69px] px-[28px] lg:px-[32px] bg-[#7D5555] justify-between">
+                    <span>LEARN MORE</span> <span>{'>'}</span>
+                  </button>
+                </Link>
+              </div>
             </div>
+          </div>
+          <div className="absolute border-y border-[#F4E8CF]  bottom-[30px] w-[120%] -left-[10%]"></div>
+        </div>
 
-            <div className="max-w-[239px] Hiragino lg:hidden text-amber-300 text-6xl font-light font-['Hiragino Mincho ProN'] tracking-[-3.022px] leading-[59.22px]">
-              Request
-              <br />
-              Pricing.
-            </div>
+        <div className="px-[30px] lg:px-0">
+          <div className="bg-[#282724]  relative z-20 space-y-[91px]  lg:space-y-[62px] p-[50px] lg:px-[109px] lg:py-[90px]">
+            <div className="space-y-[32px] lg:space-y-[54px]">
+              <div className="lg:block hidden Hiragino max-w-[734px] text-amber-300 text-[80px] font-light font-['Hiragino Mincho ProN'] leading-[82.30px]">
+                Pricing
+              </div>
 
-            <div className="max-w-[890px]  lg:block hidden text-orange-100 text-[45px] font-light font-['Gibson'] leading-[60px]">
-              Pricing for staff, supervisors and building link support is
-              provided upon request and is based on for community size and
-              needs.{' '}
-            </div>
-            <div className="lg:hidden max-w-[277px]">
-              <span className="text-yellow-50 text-xl font-light font-['Gibson'] leading-[35px] tracking-[0.6px]">
+              <div className="max-w-[239px] Hiragino lg:hidden text-amber-300 text-6xl font-light font-['Hiragino Mincho ProN'] tracking-[-3.022px] leading-[59.22px]">
+                Request
+                <br />
+                Pricing.
+              </div>
+
+              <div className="max-w-[890px]  lg:block hidden text-orange-100 text-[45px] font-light font-['Gibson'] leading-[60px]">
                 Pricing for staff, supervisors and building link support is
                 provided upon request and is based on for community size and
-                needs.
-                <br />
-              </span>
-              <span
-                onClick={mailTo}
-                className="text-yellow-50 text-xl font-semibold font-['Gibson'] leading-[35px] tracking-[0.6px]"
-              >
-                Request Pricing for a Custom Proposal.{' '}
-              </span>
+                needs.{' '}
+              </div>
+              <div className="lg:hidden max-w-[277px]">
+                <span className="text-yellow-50 text-xl font-light font-['Gibson'] leading-[35px] tracking-[0.6px]">
+                  Pricing for staff, supervisors and building link support is
+                  provided upon request and is based on for community size and
+                  needs.
+                  <br />
+                </span>
+                <span
+                  onClick={mailTo}
+                  className="text-yellow-50 text-xl font-semibold font-['Gibson'] leading-[35px] tracking-[0.6px]"
+                >
+                  Request Pricing for a Custom Proposal.{' '}
+                </span>
+              </div>
             </div>
+            <button
+              onClick={mailTo}
+              className="w-full lg:w-[397px]  flex justify-center items-center h-[69px] bg-[#F6C94B]"
+            >
+              <p className="max-w-[417.61px]  text-black text-[20px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-[0.46px]">
+                REQUEST A PROPOSAL {'>'}
+              </p>
+            </button>
           </div>
-          <button
-            onClick={mailTo}
-            className="w-full lg:w-[397px]  flex justify-center items-center h-[69px] bg-[#F6C94B]"
-          >
-            <p className="max-w-[417.61px]  text-black text-[20px] lg:text-[23px] font-semibold font-['Gibson'] leading-[29.70px] tracking-[0.46px]">
-              REQUEST A PROPOSAL {'>'}
-            </p>
-          </button>
         </div>
       </section>
       <div className="h-[150px] -mt-[150px] bg-[#F6C94B] w-full"></div>
